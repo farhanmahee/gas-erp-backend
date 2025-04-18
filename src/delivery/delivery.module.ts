daef26365fdc1c8340b4e6776b3b401cf234e397
farhanmahee <sadiulislammahee@gmail.com>
2025-04-18 18:27:01 +0600

    feat: add DeliveryGateway to providers in DeliveryModule

--- src/delivery/delivery.module.ts
+++ src/delivery/delivery.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeliveryService } from './delivery.service';
 import { DeliveryController } from './delivery.controller';
+import { DeliveryGateway } from './delivery.gateway';
 import { Delivery } from '../entity/Delivery';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Delivery]), ConfigModule],
-  providers: [DeliveryService],
+  providers: [DeliveryService, DeliveryGateway],
   controllers: [DeliveryController],
   exports: [DeliveryService],
 })
